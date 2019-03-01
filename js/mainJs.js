4820d80a58d0d28f008863a1670498cd1c5e19cd
minskmetro <42449022+minskmetro@users.noreply.github.com>
2019-03-01 20:00:15 +0300

    upd

--- js/mainJs.js
+++ js/mainJs.js
@@ -1,12 +1,3 @@
-var sideMenuLink = document.getElementsByClassName('side_menu_link')[0];
-var sideMenu = document.getElementById('side_menu');
-
-console.log(sideMenuLink);
-console.log(sideMenu);
-
-
-
-
 function sideMenuTweak () {
 
     var linkLine1 = document.getElementsByClassName('menu_link_line_st')[0];
@@ -15,36 +6,22 @@ function sideMenuTweak () {
     var menuLink = document.getElementsByClassName('side_menu_link')[0];
     var menuLinkWidth = menuLink.offsetWidth + 'px';
 
-
     if (sideMenu.style.right != menuLinkWidth){
         sideMenu.style.right = menuLinkWidth;
-        // sideMenu.style.boxShadow = '0 0 10px 0';
         linkLine1.classList.add('menu_link_rotated_st');
         linkLine2.classList.add('menu_link_rotated_nd');
     }
     else{
-        sideMenu.style.right = '100%';
-        // sideMenu.style.boxShadow = '0 0 0 0';
         linkLine1.classList.remove('menu_link_rotated_st');
         linkLine2.classList.remove('menu_link_rotated_nd');
     }
 }
 
-if (window.devicePixelRatio !== 1) { // Костыль для определения иных устройств, с коэффициентом отличным от 1
+if (window.devicePixelRatio !== 1) {
     var dpt = window.devicePixelRatio;
     var widthM = window.screen.width * dpt;
     var widthH = window.screen.height * dpt;
-
-
-
-
-    // document.write('<meta name="viewport" content="width=' + widthM+ ', height=' + widthH + '">');
-    // document.write('<meta name="viewport" content="initial-scale= ' + 1 / dpt +' ">');
     document.write('<meta name="viewport" content="initial-scale= ' + 1 / dpt + ', maximum-scale= ' + 1 / dpt + ', minimum-scale= ' + 1 / dpt + '">');
-
-    // window.devicePixelRatio = dpt/dpt;
-    // alert('native dpt (ignore this text) ' + dpt);
-    // alert('artificial dpt (ignore this text) ' + 1 / dpt);
 };
 
 window.onload = function () {
@@ -52,28 +29,6 @@ window.onload = function () {
     preloader.style.opacity = '0';
     preloader.style.visibility = 'hidden';
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 window.addEventListener("keydown", function (e) {
     if (e.keyCode == 71){
