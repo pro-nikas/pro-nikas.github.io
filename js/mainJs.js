0bb8b1eabb4daa54eb6aac8f2d0cbd05d94459d8
minskmetro <42449022+minskmetro@users.noreply.github.com>
2019-03-07 20:32:11 +0300

    upd

--- js/mainJs.js
+++ js/mainJs.js
@@ -36,3 +36,15 @@ window.addEventListener("keydown", function (e) {
         alert('Ну и зачем ты нажал эту кнопку?');
     }
 });
+
+function showBigPhoto(e) {
+    var bigPhotoWindow = document.getElementsByClassName("big_photo_window")[0];
+    if (bigPhotoWindow.style.display != 'flex' && window.innerWidth > window.innerHeight){
+        bigPhotoWindow.style.display = 'flex';
+        bigPhotoWindow.style.backgroundImage = 'url(../imgs/nature-' + e + '-2280x1380.jpg)';
+        console.info(window.innerWidth)
+    }
+    else{
+        bigPhotoWindow.style.display = 'none';
+    }
+}
